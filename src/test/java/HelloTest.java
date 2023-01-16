f67e85e8875bb65fe300b516497ad6373f91591a
serkanyildirimer <serkantitx@gmail.com>
2023-01-16 17:51:37 +0300

    Added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,7 @@ public class HelloTest {
 
     public static void main(String[] args) {
         System.out.println("Hello Test");
+        System.out.println("About the share project on github");
+
     }
 }
